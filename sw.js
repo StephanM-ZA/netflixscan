a51cfc6598bfcd1b22c2b7a80fc476cd8648964b
Stephan Marais <smarais.za@gmail.com>
2026-08-18 15:04:21 +0200

    refresh

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* netflixscan offline cache - version b0bc78893bd9
+/* netflixscan offline cache - version 8ea903dfe4b1
    Built by 'netflixscan.py publish'. Do not edit by hand; republishing
    regenerates it with a new version, which is what evicts the old copy. */
-const VERSION = "b0bc78893bd9";
+const VERSION = "8ea903dfe4b1";
 const CACHE = "netflixscan-" + VERSION;
 const SHELL = ["./", "./index.html", "./manifest.webmanifest",
                "./icon-180.png", "./icon-192.png", "./icon-512.png",
